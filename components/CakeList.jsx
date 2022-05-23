83d8c135577357de54aedb4316fd727fa1fe8bac
MathiarasiNagaraj <mathiarasinagaraj05@gmail.com>
2022-05-23 09:20:43 +0530

    api work done

---
+++ components/CakeList.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/cakeList.module.css";
+import CakeCard from "./CakeCard";
+
+const CakeList = ({ cakeList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Our Delicious</h1>
+      <p className={styles.desc}>
+      
+      </p>
+      <div className={styles.wrapper}>
+        {cakeList.map((cake) => (
+          <CakeCard key={cake._id} cake={cake} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CakeList;
---
+++ components/Footer.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/profilecake.jpg" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            We make best Home made cakes
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Contact Us</h1>
+          <p className={styles.text}>
+            1654 R. Don Road #304.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+         
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 10:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 11:00 – 6:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
